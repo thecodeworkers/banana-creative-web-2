3d659009247095267fa8efea8e6692d9806ad8cf
MauriceMiot <maurice.yggdrasil@gmail.com>
2022-11-12 17:40:30 -0400

    new field in whatsaap 'delivery '

--- src/components/ClientForm/elements/PaymentMethod/index.tsx
+++ src/components/ClientForm/elements/PaymentMethod/index.tsx
@@ -4,11 +4,13 @@ import { useSelector, useDispatch } from 'react-redux'
 import { setUserData } from '@store/actions'
 
 const methods = ['Zelle', 'Pago Móvil', 'Efectivo', 'Transferencia']
+const deliveryMethod = ['Pick-up: Torre IASA, La Castellana, Caracas', 'Envío nacional', 'Delivery']
 
 const PaymentMethod = () => {
 
   const dispatch = useDispatch()
   const [currentItem, setCurrentItem] = useState(0)
+  const [currentDelivery, setCurrentDelivery] = useState(0)
   const { userData } = useSelector((state: any) => state)
 
   const selectedItem = (item: string, index: number) => {
@@ -16,20 +18,49 @@ const PaymentMethod = () => {
     setCurrentItem(index)
   }
 
+  const selectedDelivery = (item: string, index: number) => {
+    dispatch(setUserData({ userData: { ...userData.userData, ...{ delivery: item } } }))
+    setCurrentDelivery(index)
+  }
+
   return (
-    <div>
-      <h1 className={styles._title}> Forma de pago </h1>
-      {
-        methods.map((item, index) => (
-          <div className={currentItem == index ? styles._cardSelected : styles._card} key={index} onClick={() => selectedItem(item, index)}>
-            <div className={currentItem == index ? styles._circleSelected : styles._circle}>
-              <div className={currentItem == index ? styles._childCircleSelected : styles._childCircle}>
+    <div className={styles._mainContainer}>
+
+
+      <div className={styles._sidesContainer}>
+        <div className={styles._content}>
+          <h1 className={styles._title}> Forma de pago </h1>
+          {
+            methods.map((item, index) => (
+              <div className={currentItem == index ? styles._cardSelected : styles._card} key={index} onClick={() => selectedItem(item, index)}>
+                <div className={currentItem == index ? styles._circleSelected : styles._circle}>
+                  <div className={currentItem == index ? styles._childCircleSelected : styles._childCircle}>
+                  </div>
+                </div>
+                <p className={currentItem == index ? styles._selectedText : styles._text}>{item}</p>
               </div>
-            </div>
-            <p className={currentItem == index ? styles._selectedText : styles._text}>{item}</p>
-          </div>
-        ))
-      }
+            ))
+          }
+
+        </div>
+
+        <div className={styles._contentRight}>
+          <h1 className={styles._title}> Forma de Entrega </h1>
+          {
+            deliveryMethod.map((item, index) => (
+              <div className={currentDelivery == index ? styles._cardSelected : styles._card} key={index} onClick={() => selectedDelivery(item, index)}>
+                <div className={currentDelivery == index ? styles._circleSelected : styles._circle}>
+                  <div className={currentDelivery == index ? styles._childCircleSelected : styles._childCircle}>
+                  </div>
+                </div>
+                <p className={currentDelivery == index ? styles._selectedText : styles._text}>{item}</p>
+              </div>
+            ))
+          }
+
+        </div>
+
+      </div>
     </div>
   )
 }
